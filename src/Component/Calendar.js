2e7d198c0729da5e1eab4eeb36194e8a6aaf971c
Maryam <ghorbanimoghaddam.m@gmail.com>
2021-09-30 21:27:57 +0300

    delete extra code add eventlistener for change first day of week

--- src/Component/Calendar.js
+++ src/Component/Calendar.js
@@ -1,78 +1,61 @@
 import React , { useState } from 'react';
 import Dropdown from 'react-bootstrap/Dropdown';
-import Table from 'react-bootstrap/Table';
-import {
-  format,
-  subMonths,
-  addMonths,
-  startOfWeek,
-  addDays,
-  isSameDay,
-  lastDayOfWeek,
-  getWeek,
-  addWeeks,
-  subWeeks
-} from "date-fns";
+import { format, startOfWeek, addDays, getWeek, addWeeks, subWeeks } from "date-fns";
 
 // import css
 import './CSS/Calendar.css'
 
-// import components
-import DayEvents from "./DayEvents";
-import CalendarHeader from "./CalendarHeader";
+// // import components
+// import DayEvents from "./DayEvents";
 
-const Calendar = ( ) => {
+
+const Calendar = (  ) => {
 
 	const dateFormat = "MMM yyyy";
 
 	const [currentMonth, setCurrentMonth] = useState(new Date());
+	const [weekStartDay, setweekStartDay] = useState(startOfWeek(currentMonth));
 	const [currentWeek, setCurrentWeek] = useState(getWeek(currentMonth));
 
 
-	const changeMonthHandle = (btnType) => {
-	    if (btnType === "prev") {
-	      setCurrentMonth(subMonths(currentMonth, 1));
-	    }
-	    if (btnType === "next") {
-	      setCurrentMonth(addMonths(currentMonth, 1));
-	    }
-	  };
 	const changeWeekHandle = (btnType) => {
-	    //console.log("current week", currentWeek);
+	    
 	    if (btnType === "prev") {
-	      //console.log(subWeeks(currentMonth, 1));
+	      
 	      setCurrentMonth(subWeeks(currentMonth, 1));
 	      setCurrentWeek(getWeek(subWeeks(currentMonth, 1)));
 	    }
 	    if (btnType === "next") {
-	      //console.log(addWeeks(currentMonth, 1));
+	      
 	      setCurrentMonth(addWeeks(currentMonth, 1));
 	      setCurrentWeek(getWeek(addWeeks(currentMonth, 1)));
 	    }
 	  };
 
+	  const changeStartDay = (numberOfDay) => {
+	  	  setweekStartDay(startOfWeek(currentMonth, { weekStartsOn: numberOfDay }))
+	  }
 
-	 const renderDays = () => {
+
+	 const Days = (numberOfDay) => {
 	    const dateFormat = "d MM yyyy ccc";
 	    const days = [];
 	    let startDate = startOfWeek(currentMonth, { weekStartsOn: 1 });
 	    for (let i = 0; i < 7; i++) {
 	      days.push(
-	        <div className="col col-center" key={i}>
+	        <div className="col" key={i}>
 	          {format(addDays(startDate, i), dateFormat)}
 	        </div>
 	      );
 	    }
-	    return <div className="days row">{days}</div>;
+	    return <div className="row">{days}</div>;
 	  };
 
 
 	return (
 		  <div id="calendar">
-		      {/* {renderHeader()} */}
 		      
-		      {/* {renderCells()} */}
-		      {/* {renderFooter()} */}
+		      
 			<div class="calendar-header">
 		
 				<div class="header-left">
@@ -82,13 +65,13 @@ const Calendar = ( ) => {
 					  </Dropdown.Toggle>
 
 					  <Dropdown.Menu>
-					    <Dropdown.Item href="#/action-1">Monday</Dropdown.Item>
-					    <Dropdown.Item href="#/action-2">Tuesday</Dropdown.Item>
-					    <Dropdown.Item href="#/action-3">Wednesday</Dropdown.Item>
-					    <Dropdown.Item href="#/action-4">Thursday</Dropdown.Item>
-					    <Dropdown.Item href="#/action-5">Friday</Dropdown.Item>
-					    <Dropdown.Item href="#/action-6">Saturday</Dropdown.Item>
-					    <Dropdown.Item href="#/action-7">Sunday</Dropdown.Item>
+					    <Dropdown.Item onClick={() => changeStartDay(1)}>Monday</Dropdown.Item>
+					    <Dropdown.Item onClick={()=> changeStartDay(2)}>Tuesday</Dropdown.Item>
+					    <Dropdown.Item onClick={()=> changeStartDay(3)}>Wednesday</Dropdown.Item>
+					    <Dropdown.Item onClick={()=> changeStartDay(4)}>Thursday</Dropdown.Item>
+					    <Dropdown.Item onClick={()=> changeStartDay(5)}>Friday</Dropdown.Item>
+					    <Dropdown.Item onClick={()=> changeStartDay(6)}>Saturday</Dropdown.Item>
+					    <Dropdown.Item onClick={()=> changeStartDay(0)}>Sunday</Dropdown.Item>
 					  </Dropdown.Menu>
 					</Dropdown>
 				</div>
@@ -105,7 +88,7 @@ const Calendar = ( ) => {
 				</div>
 					
 			</div>
-			<div className="days">{renderDays()}</div>
+			<div className="days">{Days()}</div>
 
 		</div>
 		);
